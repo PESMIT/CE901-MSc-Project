54e677a9ea1e166fbcb29a793d179050c801c75f
PESMIT <32641574+PESMIT@users.noreply.github.com>
2018-05-30 16:32:21 +0100

    30-05-18 - Added in Trigger Collider Platforms - New Assets Pack(Unused)
    Reminder: "OnTriggerEnter" not "onTriggerEnter"
    
    + New Collider (Not yet Prefab)
    + New Collider Light
    + New Collider Trigger
    + Two Platforms
    + New Script (Collection Script)

---
+++ CE901 - Unity Project/CE901 - Project/Assets/Scripts/Collection_Trigger.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collection_Trigger : MonoBehaviour {
+
+	GameObject lightBodyOrange, lightBodyRed;
+	Light cLightOrange, cLightRed;
+
+	// Use this for initialization
+	void Start () {
+		lightBodyOrange = GameObject.Find ("Orange_Platform_Light");
+		cLightOrange = lightBodyOrange.GetComponent<Light> ();
+
+		lightBodyRed = GameObject.Find ("Red_Platform_Light");
+		cLightRed = lightBodyRed.GetComponent<Light> ();
+
+		print ("I have found the game object");
+		cLightOrange.color = Color.blue;
+		cLightRed.color = Color.blue;
+	}
+
+	void OnTriggerStay(Collider other){
+		if (other.tag == "Objects") {
+			if (this.gameObject.name == "Orange_Platform") {
+				cLightOrange.color = Color.green;
+				cLightRed.color = Color.red;
+			} else {
+				cLightOrange.color = Color.red;
+				cLightRed.color = Color.green;
+			}
+		}
+	}
+
+	void OnTriggerExit(Collider other){
+		if (other.tag == "Objects") {
+			cLightOrange.color = Color.white;
+			cLightRed.color = Color.white;
+		}
+	}
+}
