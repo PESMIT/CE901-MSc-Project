3c9b099b60d2aef75c8c6b6acd5850de365f4abb
PESMIT <32641574+PESMIT@users.noreply.github.com>
2018-05-31 14:50:37 +0100

    31-05-18 - Added in First Scenario Scene - Added in Teleporter Prefab - Adjusted Names
    First Scenario still needs a GUI - but now teleporter activates on choice made.

--- CE901 - Unity Project/CE901 - Project/Assets/Scripts/Collection_Trigger.cs
+++ CE901 - Unity Project/CE901 - Project/Assets/Scripts/Collection_Trigger.cs
@@ -4,38 +4,55 @@ using UnityEngine;
 
 public class Collection_Trigger : MonoBehaviour {
 
-	GameObject lightBodyOrange, lightBodyRed;
-	Light cLightOrange, cLightRed;
+	GameObject platformBody1, platformBody2;
+	Light platformLight1, platformLight2;
+	private bool platform2Exists;
+	public bool platform1Chosen, platform2Chosen;
 
 	// Use this for initialization
 	void Start () {
-		lightBodyOrange = GameObject.Find ("Orange_Platform_Light");
-		cLightOrange = lightBodyOrange.GetComponent<Light> ();
-
-		lightBodyRed = GameObject.Find ("Red_Platform_Light");
-		cLightRed = lightBodyRed.GetComponent<Light> ();
-
-		print ("I have found the game object");
-		cLightOrange.color = Color.blue;
-		cLightRed.color = Color.blue;
+		if (GameObject.Find ("Platform_1")!= null) {
+			platformBody1 = GameObject.Find ("Platform_1");
+			platformLight1 = platformBody1.GetComponentInChildren<Light> ();
+			platformLight1.color = Color.blue;
+		}
+		if (GameObject.Find ("Platform_2")!= null) {
+			platformBody2 = GameObject.Find ("Platform_2");
+			platformLight2 = platformBody2.GetComponentInChildren<Light> ();
+			platformLight2.color = Color.blue;
+			platform2Exists = true;
+		}
+			
 	}
 
 	void OnTriggerStay(Collider other){
 		if (other.tag == "Objects") {
-			if (this.gameObject.name == "Orange_Platform") {
-				cLightOrange.color = Color.green;
-				cLightRed.color = Color.red;
+			if (this.gameObject.name == "Platform_1") {
+				platformLight1.color = Color.green;
+				platform1Chosen = true;
+				if (platform2Exists) {
+					platformLight2.color = Color.red;
+					platform2Chosen = false;
+				}
 			} else {
-				cLightOrange.color = Color.red;
-				cLightRed.color = Color.green;
+				platformLight1.color = Color.red;
+				platform1Chosen = false;
+				if (platform2Exists) {
+					platformLight2.color = Color.green;
+					platform2Chosen = true;
+				}
 			}
 		}
 	}
 
 	void OnTriggerExit(Collider other){
 		if (other.tag == "Objects") {
-			cLightOrange.color = Color.white;
-			cLightRed.color = Color.white;
+			platformLight1.color = Color.white;
+			platform1Chosen = false;
+			if (platform2Exists) {
+				platformLight2.color = Color.white;
+				platform2Chosen = false;
+			}
 		}
 	}
 }
---
+++ CE901 - Unity Project/CE901 - Project/Assets/Teleporter_Script.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter_Script : MonoBehaviour {
+
+	GameObject platform1, platform2, player;
+	Collection_Trigger choice1, choice2;
+	Material m_Material;
+
+	// Use this for initialization
+	void Start () {
+		platform1 = GameObject.Find ("Platform_1");
+		platform2 = GameObject.Find ("Platform_2");
+		player = GameObject.Find ("Player");
+		choice1 = platform1.GetComponent<Collection_Trigger> ();
+		choice2 = platform2.GetComponent<Collection_Trigger> ();
+		m_Material = GetComponent<Renderer> ().material;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (choice1.platform1Chosen) {
+			m_Material.color = Color.green;
+		} else if (choice1.platform2Chosen) {
+			m_Material.color = Color.green;
+		} else if (choice2.platform1Chosen) {
+			m_Material.color = Color.green;
+		} else if (choice2.platform2Chosen) {
+			m_Material.color = Color.green;
+		} else {
+			m_Material.color = Color.black;
+		}
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (choice1.platform1Chosen && !choice2.platform2Chosen) {
+			if (other.name == "Player_Position") {
+				Debug.Log ("Platform 1 Chosen - Now Teleporting");
+				player.transform.position = new Vector3 (0, 7);
+			}
+		}
+		else if (!choice1.platform1Chosen && choice2.platform2Chosen) {
+			if (other.name == "Player_Position") {
+				Debug.Log ("Platform 2 Chosen - Now Teleporting");
+				player.transform.position = new Vector3 (0, 7);
+			}
+		}
+	}
+}
