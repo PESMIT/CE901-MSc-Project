6ae6c398384bc439bdf60c6ea5f72bd2da9cf4a0
PESMIT <32641574+PESMIT@users.noreply.github.com>
2018-06-05 22:20:11 +0100

    Added in Main Menu Scene - Changed First Scenario Lighting to Non-Automatic

---
+++ CE901 - Unity Project/CE901 - Project/Assets/Scripts/Scene_Manager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene_Manager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public void onClick(int level){
+		if (level == 0) {
+			SceneManager.LoadScene ("FirstScenario");
+		} else {
+			Debug.Log ("Incorrect Scene Number to load");
+		}
+	}
+
+	public void onQuit(){
+		Application.Quit ();
+	}
+}
