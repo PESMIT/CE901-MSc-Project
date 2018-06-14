4d969ff6455d46f1331d67f190537612b69cf8be
PESMIT <32641574+PESMIT@users.noreply.github.com>
2018-06-14 15:19:38 +0100

    Sprint Session 5 - Alpha Build Part 1 - 14-06-18
    - Finished Scenario 1 Landscape -
    - Finished Scenario 1 Coding -
    - Scenario 2 has been code-updated and no longer conflicts with Scenario 1 -
    - Scenario 2 now has Option Placards and Spotlights -
    - Scenario 2 now has a working teleporter to Scenario 3 -
    - Scenario 3 has no current landscape or objective -
    - TODO: Canvas Objective Question for Scenario 2 -

--- CE901 - Unity Project/CE901 - Project/Assets/Scripts/Collection_Trigger.cs
+++ CE901 - Unity Project/CE901 - Project/Assets/Scripts/Collection_Trigger.cs
@@ -4,14 +4,20 @@ using UnityEngine;
 
 public class Collection_Trigger : MonoBehaviour {
 
+	// GENERIC ASSETS
+	CanvasScript csScript;
+
 	// ROOM ONE ASSETS
 	GameObject platformBody_R1_1, platformBody_R1_2;
-	CanvasScript csScript;
 	Light platformLight_R1_1, platformLight_R1_2;
-	private bool platform2Exists;
-	public bool platform1Chosen, platform2Chosen;
+	private bool platform_R1_2_Exists;
+	public bool platform_R1_1_Chosen, platform_R1_2_Chosen;
 
-	// TODO: ROOM TWO ASSETS
+	// ROOM TWO ASSETS
+	GameObject platformBody_R2_1, platformBody_R2_2;
+	Light platformLight_R2_1, platformLight_R2_2;
+	private bool platform_R2_2_Exists;
+	public bool platform_R2_1_Chosen, platform_R2_2_Chosen;
 
 	// Use this for initialization
 	void Start () {
@@ -29,10 +35,23 @@ public class Collection_Trigger : MonoBehaviour {
 			platformBody_R1_2 = GameObject.Find ("Platform_R1_2");
 			platformLight_R1_2 = platformBody_R1_2.GetComponentInChildren<Light> ();
 			platformLight_R1_2.color = Color.blue;
-			platform2Exists = true;
+			platform_R1_2_Exists = true;
 		}
 
 		// TODO: ROOM TWO ASSET REFERENCE
+		// Platform 1 - RIGHT SIDE
+		if(GameObject.Find("Platform_R2_1")!= null){
+			platformBody_R2_1 = GameObject.Find ("Platform_R2_1");
+			platformLight_R2_1 = platformBody_R2_1.GetComponentInChildren<Light> ();
+			platformLight_R2_1.color = Color.blue;
+		}
+		// Platform 2 - LEFT SIDE
+		if(GameObject.Find("Platform_R2_2")!= null){
+			platformBody_R2_2 = GameObject.Find ("Platform_R2_2");
+			platformLight_R2_2 = platformBody_R2_2.GetComponentInChildren<Light> ();
+			platformLight_R2_2.color = Color.blue;
+			platform_R2_2_Exists = true;
+		}
 			
 	}
 
@@ -42,37 +61,63 @@ public class Collection_Trigger : MonoBehaviour {
 		if (other.tag == "Objects") {
 			if (this.gameObject.name == "Platform_R1_1") {
 				platformLight_R1_1.color = Color.green;
-				platform1Chosen = true;
-				if (platform2Exists) {
+				platform_R1_1_Chosen = true;
+				if (platform_R1_2_Exists) {
 					platformLight_R1_2.color = Color.red;
-					platform2Chosen = false;
+					platform_R1_2_Chosen = false;
 				}
-			} else {
+			} else if(this.gameObject.name == "Platform_R1_2"){
 				platformLight_R1_1.color = Color.red;
-				platform1Chosen = false;
-				if (platform2Exists) {
+				platform_R1_1_Chosen = false;
+				if (platform_R1_2_Exists) {
 					platformLight_R1_2.color = Color.green;
-					platform2Chosen = true;
+					platform_R1_2_Chosen = true;
 				}
 			}
 		}
 
 		// TODO: ROOM TWO - TRIGGER COLLISIONS
+		if(other.tag == "Objects"){
+			if (this.gameObject.name == "Platform_R2_1") {
+				platformLight_R2_1.color = Color.green;
+				platform_R2_1_Chosen = true;
+				if (platform_R2_2_Exists) {
+					platformLight_R2_2.color = Color.red;
+					platform_R2_2_Chosen = false;
+				}
+			} else if(this.gameObject.name == "Platform_R2_2"){
+				platformLight_R2_1.color = Color.red;
+				platform_R2_1_Chosen = false;
+				if (platform_R2_2_Exists) {
+					platformLight_R2_2.color = Color.green;
+					platform_R2_2_Chosen = true;
+				}
+			}
+		}
 	}
 
 	void OnTriggerExit(Collider other){
 
 		// ROOM ONE - TRIGGER EXITS
-		if (other.tag == "Objects") {
+		if (other.tag == "Objects" && this.gameObject.name == "Platform_R1_1" || this.gameObject.name == "Platform_R1_2") {
 			csScript.RemoveTeleporterCanvas ();
 			platformLight_R1_1.color = Color.white;
-			platform1Chosen = false;
-			if (platform2Exists) {
+			platform_R1_1_Chosen = false;
+			if (platform_R1_2_Exists) {
 				platformLight_R1_2.color = Color.white;
-				platform2Chosen = false;
+				platform_R1_2_Chosen = false;
 			}
 		}
 
 		// TODO: ROOM TWO - TRIGGER EXITS
+		if(other.tag == "Objects" && this.gameObject.name == "Platform_R2_1" || this.gameObject.name == "Platform_R2_2"){
+			csScript.RemoveTeleporterCanvas ();
+			platformLight_R2_1.color = Color.white;
+			platform_R2_1_Chosen = false;
+			if (platform_R2_2_Exists) {
+				platformLight_R2_2.color = Color.white;
+				platform_R2_2_Chosen = false;
+			}
+		}
 	}
 }
--- CE901 - Unity Project/CE901 - Project/Assets/Scripts/Teleporter_Script.cs
+++ CE901 - Unity Project/CE901 - Project/Assets/Scripts/Teleporter_Script.cs
@@ -5,16 +5,20 @@ using UnityEngine;
 public class Teleporter_Script : MonoBehaviour {
 
 	// Room One Assets
-	GameObject platform_R1_1, platform_R1_2, teleporterPad_R1_1, sphere_R1_1;
-	Collection_Trigger choice1, choice2;
+	GameObject platform_R1_1, platform_R1_2, teleporter_R1_1,teleporterPad_R1_1, sphere_R1_1;
+	Collection_Trigger choice_R1_1, choice_R1_2;
+	bool tele_R1;
 
 	// Room Two Assets
-	GameObject platform_R2_1, platform_R2_2, teleporter_R2_1, sphere_R2_1;
+	GameObject platform_R2_1, platform_R2_2, teleporter_R2_1,teleporterPad_R2_1, sphere_R2_1;
+	Collection_Trigger choice_R2_1, choice_R2_2;
+	bool tele_R2;
 
 	// Generic Assets
 	GameObject player;
 	CanvasScript playerCS;
-	Material m_Material;
+	Material teleMaterial_R1_1;
+	Material teleMaterial_R2_1;
 
 	// Use this for initialization
 	void Start () {
@@ -24,51 +28,62 @@ public class Teleporter_Script : MonoBehaviour {
 		// Room One Assets
 		platform_R1_1 = GameObject.Find ("Platform_R1_1");
 		platform_R1_2 = GameObject.Find ("Platform_R1_2");
+		teleporter_R1_1 = GameObject.Find ("Teleporter_R1_1");
 		teleporterPad_R1_1 = GameObject.Find ("TelePad_R1_1");
 		sphere_R1_1 = GameObject.Find ("Sphere_R1_1");
-		choice1 = platform_R1_1.GetComponent<Collection_Trigger> ();
-		choice2 = platform_R1_2.GetComponent<Collection_Trigger> ();
+		choice_R1_1 = platform_R1_1.GetComponent<Collection_Trigger> ();
+		choice_R1_2 = platform_R1_2.GetComponent<Collection_Trigger> ();
+
+		// Room One Teleporter Materia
+		teleMaterial_R1_1 = GameObject.Find("Teleporter_R1_1").GetComponent<Renderer> ().material;
 
 		// Room Two Assets
 		platform_R2_1 = GameObject.Find("Platform_R2_1");
 		platform_R2_2 = GameObject.Find("Platform_R2_2");
+		teleporter_R2_1 = GameObject.Find ("Teleporter_R2_1");
+		teleporterPad_R2_1 = GameObject.Find ("TelePad_R2_1");
+		sphere_R2_1 = GameObject.Find ("Sphere_R2_1");
+		choice_R2_1 = platform_R2_1.GetComponent<Collection_Trigger> ();
+		choice_R2_2 = platform_R2_2.GetComponent<Collection_Trigger> ();
 
+		// Room Two Teleporter Materia
+		teleMaterial_R2_1 = GameObject.Find("Teleporter_R2_1").GetComponent<Renderer> ().material;
 
-		m_Material = GetComponent<Renderer> ().material;
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
 		// Room One Assets - Material Updates
-		if (choice1.platform1Chosen || choice1.platform2Chosen || choice2.platform1Chosen || choice2.platform2Chosen) {
+		if (choice_R1_1.platform_R1_1_Chosen || choice_R1_1.platform_R1_2_Chosen || choice_R1_2.platform_R1_1_Chosen || choice_R1_2.platform_R1_2_Chosen) {
 			if (!playerCS.teleTextCorrect) {playerCS.DisplayTeleporterCorrect ();}
+			teleMaterial_R1_1.color = Color.green;
 		}
-
-		if (choice1.platform1Chosen || choice1.platform2Chosen || choice2.platform1Chosen || choice2.platform2Chosen) {
-			m_Material.color = Color.green;
-		} 
-		else {m_Material.color = Color.black;}
+		else {teleMaterial_R1_1.color = Color.black;}
 
 		// TODO: Room Two Assets - Material Updates
+		if(choice_R2_1.platform_R2_1_Chosen || choice_R2_1.platform_R2_2_Chosen || choice_R2_2.platform_R2_1_Chosen || choice_R2_2.platform_R2_2_Chosen) {
+			if (!playerCS.teleTextCorrect) {playerCS.DisplayTeleporterCorrect ();}
+			teleMaterial_R2_1.color = Color.green;
+		} 
+		else {teleMaterial_R2_1.color = Color.black;}
 	}
 
 	void OnTriggerEnter(Collider other){
 
 		// Room One Assets - Collision with Player Hitbox
-
-		if (choice1.platform1Chosen && !choice2.platform2Chosen) {
+		if (choice_R1_1.platform_R1_1_Chosen && !choice_R1_2.platform_R1_2_Chosen) {
 			if (other.name == "Player_Position") {
-				Debug.Log ("Platform 1 Chosen - Now Teleporting");
+				Debug.Log ("Platform R1_1 Chosen - Now Teleporting");
 				player.transform.position = teleporterPad_R1_1.transform.position;
 				playerCS.RemoveTeleporterCanvas ();
 				sphere_R1_1.transform.position = Vector3.zero;
 				Object.Destroy (sphere_R1_1, 0.1f);
 			}
 		}
-		else if (!choice1.platform1Chosen && choice2.platform2Chosen) {
+		else if (!choice_R1_1.platform_R1_1_Chosen && choice_R1_2.platform_R1_2_Chosen) {
 			if (other.name == "Player_Position") {
-				Debug.Log ("Platform 2 Chosen - Now Teleporting");
+				Debug.Log ("Platform R1_2 Chosen - Now Teleporting");
 				player.transform.position = teleporterPad_R1_1.transform.position;
 				playerCS.RemoveTeleporterCanvas ();
 				sphere_R1_1.transform.position = Vector3.zero;
@@ -83,6 +98,26 @@ public class Teleporter_Script : MonoBehaviour {
 
 
 		// TODO : Room Two Assets - Collision with Player Hitbox
-
+		if (choice_R2_1.platform_R2_1_Chosen && !choice_R2_2.platform_R2_2_Chosen) {
+			if (other.name == "Player_Position") {
+				Debug.Log ("Platform R2_1 Chosen - Now Teleporting");
+				player.transform.position = teleporterPad_R2_1.transform.position;
+				playerCS.RemoveTeleporterCanvas ();
+				sphere_R2_1.transform.position = Vector3.zero;
+				Object.Destroy (sphere_R2_1, 0.1f);
+			}
+		} else if (!choice_R2_1.platform_R2_1_Chosen && choice_R2_2.platform_R2_2_Chosen) {
+			if (other.name == "Player_Position") {
+				Debug.Log ("Platform R2_2 Chosen - Now Teleporting");
+				player.transform.position = teleporterPad_R2_1.transform.position;
+				playerCS.RemoveTeleporterCanvas ();
+				sphere_R2_1.transform.position = Vector3.zero;
+				Object.Destroy (sphere_R2_1, 0.1f);
+			}
+		} else if (other.name == "Player_Position") {
+			if (!playerCS.teleTextIncorrect) {
+				playerCS.DisplayTeleporterIncorrect ();
+			}
+		}
 	}
 }
